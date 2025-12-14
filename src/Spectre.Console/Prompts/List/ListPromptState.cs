454865061eb3d9c568ddeee902ebd0c03c62627e
Akif <102677971+akifisitan@users.noreply.github.com>
2025-12-14 14:53:16 +0300

    feat: ListPrompt ux changes (#2)
    - Add search filter to allow custom search predicates
    - Add ability to filter searched items
    - Move search prompt to the top
    - Made it so Ctrl + Backspace deletes the whole search word
    - Add ability to abort on escape press
    - Add ability to prevent clearing output after submit

--- src/Spectre.Console/Prompts/List/ListPromptState.cs
+++ src/Spectre.Console/Prompts/List/ListPromptState.cs
@@ -5,18 +5,20 @@ internal sealed class ListPromptState<T>
 {
     private readonly Func<T, string> _converter;
 
-    public int Index { get; private set; }
-    public int ItemCount => Items.Count;
+    public int Index => _selectableItems.Count == 0 ? 0 : _selectableItems[_selectableIndex].Index;
     public int PageSize { get; }
     public bool WrapAround { get; }
     public SelectionMode Mode { get; }
     public bool SkipUnselectableItems { get; private set; }
     public bool SearchEnabled { get; }
     public IReadOnlyList<ListPromptItem<T>> Items { get; }
-    private readonly IReadOnlyList<int>? _leafIndexes;
-
-    public ListPromptItem<T> Current => Items[Index];
+    public ListPromptItem<T>? Current => _selectableItems.Count == 0 ? null : _selectableItems[_selectableIndex].Item;
     public string SearchText { get; private set; }
+    private readonly Func<T, string, bool> _searchFilter;
+    public List<ListPromptItem<T>> VisibleItems { get; private set; }
+    public bool FilterOnSearch { get; private set; }
+    private List<SelectableItem> _selectableItems;
+    private int _selectableIndex;
 
     public ListPromptState(
         IReadOnlyList<ListPromptItem<T>> items,
@@ -24,7 +26,9 @@ internal sealed class ListPromptState<T>
         int pageSize, bool wrapAround,
         SelectionMode mode,
         bool skipUnselectableItems,
-        bool searchEnabled)
+        bool searchEnabled,
+        Func<T, string, bool>? searchFilter,
+        bool filterOnSearch)
     {
         _converter = converter ?? throw new ArgumentNullException(nameof(converter));
         Items = items;
@@ -34,149 +38,161 @@ internal sealed class ListPromptState<T>
         SkipUnselectableItems = skipUnselectableItems;
         SearchEnabled = searchEnabled;
         SearchText = string.Empty;
-
-        if (SkipUnselectableItems && mode == SelectionMode.Leaf)
-        {
-            _leafIndexes =
-                Items
-                    .Select((item, index) => new { item, index })
-                    .Where(x => !x.item.IsGroup)
-                    .Select(x => x.index)
-                    .ToList()
-                    .AsReadOnly();
-
-            Index = _leafIndexes.FirstOrDefault();
-        }
-        else
-        {
-            Index = 0;
-        }
+        _searchFilter = searchFilter ?? DefaultSearchFilter;
+        FilterOnSearch = filterOnSearch;
+        VisibleItems = Items.ToList();
+        _selectableItems = GetSelectableItems();
+        _selectableIndex = 0;
     }
 
     public bool Update(ConsoleKeyInfo keyInfo)
     {
-        var index = Index;
-        if (SkipUnselectableItems && Mode == SelectionMode.Leaf)
-        {
-            Debug.Assert(_leafIndexes != null, nameof(_leafIndexes) + " != null");
-            var currentLeafIndex = _leafIndexes.IndexOf(index);
-            switch (keyInfo.Key)
-            {
-                case ConsoleKey.UpArrow:
-                case ConsoleKey.K:
-                    if (currentLeafIndex > 0)
-                    {
-                        index = _leafIndexes[currentLeafIndex - 1];
-                    }
-                    else if (WrapAround)
-                    {
-                        index = _leafIndexes.LastOrDefault();
-                    }
-
-                    break;
-
-                case ConsoleKey.DownArrow:
-                case ConsoleKey.J:
-                    if (currentLeafIndex < _leafIndexes.Count - 1)
-                    {
-                        index = _leafIndexes[currentLeafIndex + 1];
-                    }
-                    else if (WrapAround)
-                    {
-                        index = _leafIndexes.FirstOrDefault();
-                    }
-
-                    break;
-
-                case ConsoleKey.Home:
-                    index = _leafIndexes.FirstOrDefault();
-                    break;
-
-                case ConsoleKey.End:
-                    index = _leafIndexes.LastOrDefault();
-                    break;
-
-                case ConsoleKey.PageUp:
-                    index = Math.Max(currentLeafIndex - PageSize, 0);
-                    if (index < _leafIndexes.Count)
-                    {
-                        index = _leafIndexes[index];
-                    }
-
-                    break;
-
-                case ConsoleKey.PageDown:
-                    index = Math.Min(currentLeafIndex + PageSize, _leafIndexes.Count - 1);
-                    if (index < _leafIndexes.Count)
-                    {
-                        index = _leafIndexes[index];
-                    }
-
-                    break;
-            }
-        }
-        else
-        {
-            index = keyInfo.Key switch
-            {
-                ConsoleKey.UpArrow or ConsoleKey.K => Index - 1,
-                ConsoleKey.DownArrow or ConsoleKey.J => Index + 1,
-                ConsoleKey.Home => 0,
-                ConsoleKey.End => ItemCount - 1,
-                ConsoleKey.PageUp => Index - PageSize,
-                ConsoleKey.PageDown => Index + PageSize,
-                _ => Index,
-            };
-        }
-
-        var search = SearchText;
-
         if (SearchEnabled)
         {
-            // If is text input, append to search filter
             if (!char.IsControl(keyInfo.KeyChar))
             {
-                search = SearchText + keyInfo.KeyChar;
-
-                var item = Items.FirstOrDefault(x =>
-                    _converter.Invoke(x.Data).Contains(search, StringComparison.OrdinalIgnoreCase)
-                    && (!x.IsGroup || Mode != SelectionMode.Leaf));
-
-                if (item != null)
+                SearchText += keyInfo.KeyChar;
+                if (FilterOnSearch)
                 {
-                    index = Items.IndexOf(item);
+                    VisibleItems = FilterItemsBySearch();
+                    _selectableItems = GetSelectableItems();
+                    _selectableIndex = 0;
                 }
+                else
+                {
+                    var item = _selectableItems
+                        .FirstOrDefault(x => MatchesSearch(x.Item));
+                    if (item != null)
+                    {
+                        _selectableIndex = _selectableItems.IndexOf(item);
+                    }
+                }
+
+                return true;
             }
 
             if (keyInfo.Key == ConsoleKey.Backspace)
             {
-                if (search.Length > 0)
+                if (SearchText.Length > 0)
                 {
-                    search = search.Substring(0, search.Length - 1);
-                }
+                    SearchText = keyInfo.Modifiers != ConsoleModifiers.Control ? SearchText[..^1] : "";
+                    if (FilterOnSearch)
+                    {
+                        VisibleItems = FilterItemsBySearch();
+                        _selectableItems = GetSelectableItems();
+                        _selectableIndex = 0;
+                    }
+                    else
+                    {
+                        var item = _selectableItems
+                            .FirstOrDefault(x => MatchesSearch(x.Item));
+                        if (item != null)
+                        {
+                            _selectableIndex = _selectableItems.IndexOf(item);
+                        }
+                    }
 
-                var item = Items.FirstOrDefault(x =>
-                    _converter.Invoke(x.Data).Contains(search, StringComparison.OrdinalIgnoreCase) &&
-                    (!x.IsGroup || Mode != SelectionMode.Leaf));
-
-                if (item != null)
-                {
-                    index = Items.IndexOf(item);
+                    return true;
                 }
             }
         }
 
-        index = WrapAround
-            ? (ItemCount + (index % ItemCount)) % ItemCount
-            : index.Clamp(0, ItemCount - 1);
-
-        if (index != Index || SearchText != search)
+        switch (keyInfo.Key)
         {
-            Index = index;
-            SearchText = search;
-            return true;
+            case ConsoleKey.UpArrow:
+                if (_selectableIndex > 0)
+                {
+                    _selectableIndex--;
+                }
+                else if (WrapAround)
+                {
+                    _selectableIndex = _selectableItems.Count - 1;
+                }
+
+                return true;
+
+            case ConsoleKey.DownArrow:
+                if (_selectableIndex < _selectableItems.Count - 1)
+                {
+                    _selectableIndex++;
+                }
+                else if (WrapAround)
+                {
+                    _selectableIndex = 0;
+                }
+
+                return true;
+
+            case ConsoleKey.Home:
+                _selectableIndex = 0;
+                return true;
+
+            case ConsoleKey.End:
+                _selectableIndex = _selectableItems.Count - 1;
+                return true;
+
+            case ConsoleKey.PageUp:
+                var pageUpIndex = Index - PageSize;
+                if (WrapAround)
+                {
+                    pageUpIndex = (pageUpIndex + VisibleItems.Count) % VisibleItems.Count;
+                }
+                else
+                {
+                    pageUpIndex = Math.Max(pageUpIndex, 0);
+                }
+
+                _selectableIndex = _selectableItems.IndexOf(_selectableItems.First(x => x.Index >= pageUpIndex));
+                return true;
+
+            case ConsoleKey.PageDown:
+                var pageDownIndex = Index + PageSize;
+                if (WrapAround)
+                {
+                    pageDownIndex %= VisibleItems.Count;
+                }
+                else
+                {
+                    pageDownIndex = Math.Min(pageDownIndex, VisibleItems.Count - 1);
+                }
+
+                _selectableIndex = _selectableItems.IndexOf(_selectableItems.First(x => x.Index >= pageDownIndex));
+                return true;
         }
 
         return false;
+    }
+
+    private bool DefaultSearchFilter(T data, string search)
+    {
+        return _converter.Invoke(data).Contains(search, StringComparison.OrdinalIgnoreCase);
+    }
+
+    private List<SelectableItem> GetSelectableItems()
+    {
+        var selectableItems = VisibleItems
+            .Select((item, filteredIndex) => new SelectableItem(item, filteredIndex));
+
+        if (SkipUnselectableItems && Mode == SelectionMode.Leaf)
+        {
+            selectableItems = selectableItems.Where(x => !x.Item.IsGroup);
+        }
+
+        return selectableItems.ToList();
+    }
+
+    private List<ListPromptItem<T>> FilterItemsBySearch()
+    {
+        return Items
+            .Where(x => MatchesSearch(x) || x.Children.Any(MatchesSearch))
+            .ToList();
+    }
+
+    private bool MatchesSearch(ListPromptItem<T> item) => _searchFilter(item.Data, SearchText);
+
+    private class SelectableItem(ListPromptItem<T> item, int index)
+    {
+        public ListPromptItem<T> Item { get; } = item;
+        public int Index { get; } = index;
     }
 }
